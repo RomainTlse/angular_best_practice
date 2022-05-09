60d77002b8399000c02627808c55a45340160b29
Romain Montaut <montaut.romain@gmail.com>
2022-05-09 22:44:51 +0200

    feat: :sparkles: #22 Mise à jour du titre de l'application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -34,7 +34,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular_best_practice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular_best_practice');
+    expect(app.title).toEqual('Angular Best Practice');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,10 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'angular_best_practice';
+  title = 'Angular Best Practice';
 
-  constructor(private _translate: TranslateService) {
+  constructor(private _titleService: Title, private _translate: TranslateService) {
+    this._titleService.setTitle(this.title);
     _translate.setDefaultLang('fr');
     _translate.use('fr');
   }
