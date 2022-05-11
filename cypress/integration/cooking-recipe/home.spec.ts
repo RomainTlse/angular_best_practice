43a82a6ea683a6acfa7a1406988f76c8900f1bfb
Romain Montaut <montaut.romain@gmail.com>
2022-05-11 10:34:22 +0200

    fix: :bug: #52 Mise à jour variables css et traduction dans le loader

--- cypress/integration/cooking-recipe/home.spec.ts
+++ cypress/integration/cooking-recipe/home.spec.ts
@@ -2,6 +2,5 @@ describe('Home cookingrecipe Test', () => {
   it('Access Page', () => {
     cy.visit('/cookingrecipe');
     cy.contains('home works!');
-    cy.contains('Bonjour tout le monde !');
   });
 });
