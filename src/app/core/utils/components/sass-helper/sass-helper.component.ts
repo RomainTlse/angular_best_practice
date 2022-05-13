df5b5ec04db137858e303f6f142e43f062563a10
Romain Montaut <montaut.romain@gmail.com>
2022-05-13 23:21:56 +0200

    feat: :sparkles: #46 Gestion du component sass-helper

--- src/app/core/utils/components/sass-helper/sass-helper.component.ts
+++ src/app/core/utils/components/sass-helper/sass-helper.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
+export const PREFIX = '--';
+
 @Component({
   selector: 'app-sass-helper',
   templateUrl: './sass-helper.component.html',
   styleUrls: ['./sass-helper.component.sass'],
 })
-export class SassHelperComponent {}
+export class SassHelperComponent {
+  /**
+   * Read the custom property of body section with given name.
+   * @param name The property name.
+   * @returns The property value.
+   */
+  readProperty(name: string): string {
+    const bodyStyles = window.getComputedStyle(document.body);
+    return bodyStyles.getPropertyValue(PREFIX + name).trim();
+  }
+}
--- src/app/modules/cooking-recipe/pages/home/home.module.ts
+++ src/app/modules/cooking-recipe/pages/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { UtilsModule } from '../../../../core/utils/utils.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, UtilsModule],
 })
 export class HomeModule {}
