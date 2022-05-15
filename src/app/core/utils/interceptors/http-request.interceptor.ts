40e6ddd3a3d528c0e12b071741dd4a857612f037
Romain Montaut <montaut.romain@gmail.com>
2022-05-15 18:03:10 +0200

    feat: :sparkles: #47 Mise en place de l'intercepteur http

--- src/app/core/utils/interceptors/http-request.interceptor.ts
+++ src/app/core/utils/interceptors/http-request.interceptor.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { MessageService } from '../../ui/services/message.service';
+import { LoaderService } from '../../ui/services/loader.service';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+  constructor(private _messageService: MessageService, private _loaderService: LoaderService) {}
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this._loaderService.hide();
+        this._messageService.show({
+          show: true,
+          type: 'error',
+          title: 'error.title',
+          description: 'error.description',
+        });
+        return throwError(error.message);
+      })
+    );
   }
 }
